49c64465bd5a943b12572aef3d4f8f38d5c16f0c
Brigette LeVert <bnl.levert@gmail.com>
2020-02-01 00:36:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,22 +25,22 @@ class CashRegister
     #@last_transaction_amount
   end
   
-  def apply_discount()
-    if @discount > 0 
-      @discount = @discount/100.to_f 
-      @total = @total - (@total * (@discount))
-      return "After the discount, the total comes to $#{@total.to_i}."
-    else
-      return "There is no discount to apply."
-    end
-  end
+  # def apply_discount()
+  #   if @discount > 0 
+  #     @discount = @discount/100.to_f 
+  #     @total = @total - (@total * (@discount))
+  #     return "After the discount, the total comes to $#{@total.to_i}."
+  #   else
+  #     return "There is no discount to apply."
+  #   end
+  # end
   
   # def void_last_transaction
   #   @total -= @last_transaction_amount
   # end
   
-  def void_last_transaction
-    self.total = self.total - self.last_transaction_amount
-  end
+  # def void_last_transaction
+  #   self.total = self.total - self.last_transaction_amount
+  # end
   
 end
