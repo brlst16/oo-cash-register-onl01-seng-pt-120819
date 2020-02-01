82c4e6f0fe9d636b31ce148929d969af0965ee0d
Brigette LeVert <bnl.levert@gmail.com>
2020-02-01 00:33:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   
   def apply_discount()
     if @discount > 0 
-      @discount = @discount.to_f / 100
+      @discount = @discount/100.to_f 
       @total = @total - (@total * (@discount))
       return "After the discount, the total comes to $#{@total.to_i}."
     else
